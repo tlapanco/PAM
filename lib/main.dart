0b8d184aaa17fb66cf31af3f6c5f4ce25fe54f7e
tlapanco <99222342+tlapanco@users.noreply.github.com>
2025-08-06 22:57:00 +0000

    Add - Main screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,//Remove debug banner
-      title: 'Flutter Demo',
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello there..!! ', selectionColor: Colors.cyanAccent,),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                color: Colors.purple,
+                width: double.infinity,
+                height: 50,
+                alignment: Alignment.center,
+                child: Text('Mi aplicación'),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  color: Colors.grey,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        width: 200,
+                        'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png',
+                      ),
+                      Text(
+                        'Hola, soy Profesor..!',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w200,
+                          fontFamily: 'serif',
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.facebook),
+                          Icon(Icons.snapchat),
+                          Icon(Icons.messenger),
+                        ],
+                      ),
+                      FloatingActionButton(
+                        elevation: 100,
+                        onPressed: () {},
+                        child: Icon(Icons.play_arrow),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        backgroundColor: Colors.blueAccent,
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
     );
   }
